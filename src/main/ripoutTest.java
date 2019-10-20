ea4b2d520afb26ad0cfdc8b2ee0713bd5c7d9581
unknown <jmslick@gmail.com>
2019-10-19 23:42:49 -0500

    Use static version

--- src/main/ripoutTest.java
+++ src/main/ripoutTest.java
@@ -1,23 +1,15 @@
 package main;
 
-<<<<<<< Updated upstream
-import com.gdeb.ripout.workflow.Ripout;
-import com.gdeb.ripout.workflow.Workflow;
-=======
 import main.java.com.gdeb.ripout.model.Ripout;
-import main.java.com.gdeb.ripout.routing.Routing;
 import main.java.com.gdeb.ripout.workflow.Workflow;
->>>>>>> Stashed changes
 
 public class ripoutTest {
 	public static void main(String[] args) {
-		Ripout ripout = new Ripout(1, 1, true, "DRFT", 012345, "siczpak");
+		Ripout ripout = new Ripout(1, 1, "RipoutWriter", true, "STATUS", 012345, "siczpak");
 
-		Workflow workflow = new Workflow(ripout);
-		workflow.signRoutingStep(ripout);
-		workflow.signRoutingStep(ripout);
-		workflow.signRoutingStep(ripout);
-		workflow.signRoutingStep(ripout);
-		workflow.signRoutingStep(ripout);
+		Workflow.signWorkflowStep(ripout);
+		Workflow.signWorkflowStep(ripout);
+		Workflow.signWorkflowStep(ripout);
+		Workflow.signWorkflowStep(ripout);
 	}
 }
