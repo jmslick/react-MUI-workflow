3ebc11a24082510426c158e80a0f1df694c6060b
unknown <jmslick@gmail.com>
2019-10-27 23:03:17 -0500

    Remove println from loadRoutingClass().

--- src/main/java/com/gdeb/ripout/workflow/Workflow.java
+++ src/main/java/com/gdeb/ripout/workflow/Workflow.java
@@ -16,8 +16,9 @@ public class Workflow {
 	private static Routing routing;
 
 	public static void signWorkflowStep(Ripout ripout) {
-		System.out.println("sign: " + ripout.toString());
+		System.out.println("Sign off: " + ripout.toString());
 		loadRoutingClass(ripout.getRouting());
+		System.out.println("Loaded next routing object: " + routing.toString());
 		ripout.setStatus(routing.getStatus());
 		ripout.setRouting(routing.calculate());
 	}
@@ -26,8 +27,7 @@ public class Workflow {
 		final String pkg = "main.java.com.gdeb.ripout.routing.";
 		try {
 			Class clazz = Class.forName(pkg + routingName);
-			routing = (Routing) clazz.newInstance();
-			System.out.println(routing.toString());
+			routing = (Routing) clazz.newInstance();			
 		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
